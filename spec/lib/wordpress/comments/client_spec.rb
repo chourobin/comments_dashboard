1fe0ec1060023ac2f7893eaf5bd69af5bcb1cead
Robin Chou <chourobin@gmail.com>
2013-04-15 15:53:19 -0400

    tabs to spaces

--- spec/lib/wordpress/comments/client_spec.rb
+++ spec/lib/wordpress/comments/client_spec.rb
@@ -5,33 +5,33 @@ require_relative '../../../../lib/wordpress/comments/client'
 #
 describe Wordpress::Comments::Client do
 
-	let(:client) { Wordpress::Comments::Client.new "http://mashable.com/comments/feed" }
+  let(:client) { Wordpress::Comments::Client.new "http://mashable.com/comments/feed" }
 
-	describe "#initialize" do
+  describe "#initialize" do
 
-		it "stores a URL" do
-			expect(client.url).to eq "http://mashable.com/comments/feed"
-		end
+    it "stores a URL" do
+      expect(client.url).to eq "http://mashable.com/comments/feed"
+    end
 
-	end
+  end
 
-	describe "#parse" do
+  describe "#parse" do
 
-		let(:xml) { File.read(File.join('spec', 'fixtures', 'feed.xml')) }
+    let(:xml) { File.read(File.join('spec', 'fixtures', 'feed.xml')) }
 
-		let(:comments) { client.parse xml }
-		let(:comment) { comments.first }
+    let(:comments) { client.parse xml }
+    let(:comment) { comments.first }
 
-		it "extracts the link" do
-			link = "http://mashable.com/2012/07/18/ipad-early-photos/comment-page-1/#comment-18239503"
-			expect(comment[:link]).to eq link
-		end
+    it "extracts the link" do
+      link = "http://mashable.com/2012/07/18/ipad-early-photos/comment-page-1/#comment-18239503"
+      expect(comment[:link]).to eq link
+    end
 
-		it "extracts the title" do
-			title = "Comment on The Earliest Photos of Apple’s iPad Hit the Web by Fido"
-			expect(comment[:title]).to eq title
-		end
+    it "extracts the title" do
+      title = "Comment on The Earliest Photos of Apple’s iPad Hit the Web by Fido"
+      expect(comment[:title]).to eq title
+    end
 
-	end
+  end
 
 end
