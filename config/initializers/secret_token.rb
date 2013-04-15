c836ba8376f7056ee2645b4ae728279277c55360
Robin Chou <chourobin@new-host-4.home>
2013-04-15 14:33:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CommentsDashboard::Application.config.secret_key_base = 'b9054ebb56552c4316b8774dfc507c105b37db9a44919aabd24832860807b09ddf0c003f0920972808e7bbcd6ba466ba390cbb78fd00ae50e6b4c5073b6a19f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CommentsDashboard::Application.config.session_store :encrypted_cookie_store, key: '_comments_dashboard_session'
