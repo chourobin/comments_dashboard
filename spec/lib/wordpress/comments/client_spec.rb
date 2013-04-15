ea7aa7cc870e8f07855f182fdc80074ded18ac26
Robin Chou <chourobin@gmail.com>
2013-04-15 15:11:47 -0400

    added feed fixture

--- spec/lib/wordpress/comments/client_spec.rb
+++ spec/lib/wordpress/comments/client_spec.rb
@@ -1,8 +1,10 @@
 module Wordpress
 	module Comments
 		class Client
-			def initialize url
+			attr_reader :url
 
+			def initialize url
+				@url = url
 			end
 		end
 	end
@@ -13,9 +15,27 @@ end
 #
 describe Wordpress::Comments::Client do
 
-	it "stores a URL" do
-		client = Wordpress::Comments::Client.new "http://mashable.com/comments/feed"
-		expect(client.url).to eq "http://mashable.com/comments/feed"
+	describe "#initialize" do
+
+		it "stores a URL" do
+			client = Wordpress::Comments::Client.new "http://mashable.com/comments/feed"
+			expect(client.url).to eq "http://mashable.com/comments/feed"
+		end
+
+	end
+
+	describe "#parse" do
+
+		let(:xml) { File.read(File.join('spec', 'fixtures', 'feed.xml')) }
+
+		it "extracts the link" do
+			client = Wordpress::Comments::Client.new "http://mashable.com/comments/feed"
+			comments = client.parse xml
+			comment = comments.first
+			link = "http://mashable.com/2012/07/18/ipad-early-photos/comment-page-1/#comment-18239503"
+			expect(comment.link).to eq link
+		end
+
 	end
 
 end
