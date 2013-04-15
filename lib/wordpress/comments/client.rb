880ef543d1c1a336f48123cab3420688e18888fa
Robin Chou <chourobin@gmail.com>
2013-04-15 15:52:27 -0400

    convert tab to spaces

--- lib/wordpress/comments/client.rb
+++ lib/wordpress/comments/client.rb
@@ -1,30 +1,30 @@
 require 'nokogiri'
 
 module Wordpress
-	module Comments
-		class Client
-			# returns the url of the comments resource
-			attr_reader :url
+  module Comments
+    class Client
+      # returns the url of the comments resource
+      attr_reader :url
 
-			# initialize the client
-			# @param [String] url the url of the comments resource
-			def initialize url
-				@url = url
-			end
+      # initialize the client
+      # @param [String] url the url of the comments resource
+      def initialize url
+        @url = url
+      end
 
-			# parse xml
-			# @param [XML] xml the xml data
-			# @return [Array] an array of comments hashes from xml data
-			def parse xml
-				doc = Nokogiri::XML xml
-				doc.search('item').map do |doc_item|
-					item = {}
-					item[:link] = doc_item.at('link').text
-					item[:title] = doc_item.at('title').text
-					item
-				end
-			end
+      # parse xml
+      # @param [XML] xml the xml data
+      # @return [Array] an array of comments hashes from xml data
+      def parse xml
+        doc = Nokogiri::XML xml
+        doc.search('item').map do |doc_item|
+          item = {}
+          item[:link] = doc_item.at('link').text
+          item[:title] = doc_item.at('title').text
+          item
+        end
+      end
 
-		end
-	end
+    end
+  end
 end
